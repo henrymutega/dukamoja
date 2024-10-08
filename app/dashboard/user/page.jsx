c0cc9f8ad0080bd05fe5cbb3fe7f89bb5efc6ebd
henrymutega <hmutega@gmail.com>
2024-10-08 09:23:16 +0300

    Protecting pages.

---
+++ app/dashboard/user/page.jsx
@@ -0,0 +1,13 @@
+export default function UserDashboard(){
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col">
+                    <h2>Dashboard</h2>
+                    <hr />
+                    <p>...............</p>
+                </div>
+            </div>
+        </div>
+    )
+}
