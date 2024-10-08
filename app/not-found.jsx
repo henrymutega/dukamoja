f75eb91d79f9778c150aef821fd270ecb7da0d23
henrymutega <hmutega@gmail.com>
2024-10-08 09:10:59 +0300

    Handling 404 page.

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,4 +1,4 @@
-export default function Loading(){
+export default function NotFound(){
     return(
         <div className="d-flex justify-content-center align-items-center vh-100 text-primary">
             404 | Page Not Found
